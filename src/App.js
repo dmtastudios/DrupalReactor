642c777730ec39475398bcf3b78342fca3e0bcd1
David Williams <53374809+dmtastudios@users.noreply.github.com>
2022-05-18 17:53:04 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import header from './header';
 
-function App() {
+function App ()
+{
+  const name = 'DrupalReactor'
+  const x = 'false'
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Drupal 9 React.js App</h1>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet quasi, maiores laudantium dolorum delectus minus repellat, nihil ex saepe exercitationem ipsam fuga qui alias vero.</h2>
+      <h3>Hello, {name ? 'Yes' : 'No'} </h3>
     </div>
   );
 }
